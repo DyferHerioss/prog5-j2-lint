9b087a7c4fa7d6ed799f8770dac0d909f53fdf4d
DyferHerioss <freddyandriamanohiniaina@gmail.com>
2025-11-21 10:00:20 +0300

    feat: add walllet project

---
+++ Size.py
@@ -0,0 +1,5 @@
+from  enum import Enum
+class Size(Enum):
+    SMALL = "SMALL"
+    MEDIUM = "MEDIUM"
+    BIG = "BIG"
---
+++ Wallet.py
@@ -0,0 +1,18 @@
+from Size import Size
+
+class Wallet:
+    def __init__(self, color: str, size: Size, money: int, lostState: bool):
+        self.color = color
+        self.size = size
+        self.money = money
+        self.lostState = lostState
+
+    def addMoney(self, amountToAdd) -> int:
+        self.money += amountToAdd
+        return self.money
+
+    def isLost(self) -> bool:
+        return self.lostState
+
+    def checkMoney(self) -> int:
+        return self.money
